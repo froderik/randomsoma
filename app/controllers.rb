e675cfeddd6843cc11e6db58e201aac41c87771d
Fredrik Rubensson <fredrik@eldfluga.se>
2011-10-03 00:21:58 +0200

    something that works - looks and deploy left to do

--- app/controllers.rb
+++ app/controllers.rb
@@ -2,4 +2,11 @@ Randomsoma.controller do
   get :index do
     render 'index'
   end
+  
+  get :random_name do
+    soma_listen = Nokogiri::HTML open 'http://somafm.com/listen/'
+    station_names = []
+    soma_listen.css('#stations ul li p a @href').each {|el| station_names << el.value.split('/').last }
+    station_names[rand station_names.size]
+  end
 end
--- public/javascripts/somaize.js
+++ public/javascripts/somaize.js
@@ -1,5 +1,14 @@
+var somaAppears = function(){
+    $.ajax({
+        url: "random_name", 
+        success: function(station_name){
+            $('.midst').html("<iframe src=http://somafm.com/popup/?" + station_name + "/>" );
+        }  
+    });
+};
+
 $(document).ready( function(){
     $('.dabutton').click(function(){
-        
+        $('.dabutton').fadeOut(somaAppears);
     });
 });
