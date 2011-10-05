99f35e36674014a39989774a98112aed657c161d
Fredrik Rubensson <fredrik@eldfluga.se>
2011-10-05 15:48:45 +0200

    added smaller image with clicker on player page

--- public/javascripts/somaize.js
+++ public/javascripts/somaize.js
@@ -1,15 +1,20 @@
+var give_birth_to_fader = function(){
+    $('.randomizer').click(function(){
+        $('.midst').fadeOut(somaAppears);
+    });    
+}
+
 var somaAppears = function(){
     $.ajax({
         url: "/random_name", 
         success: function(station_name){
-            $('.midst').html("<iframe scrolling='no' frameborder='0' src=http://somafm.com/popup/?" + station_name + "/>" );
+            var player = "<iframe scrolling='no' frameborder='0' src=http://somafm.com/popup/?" + station_name + "/>" ;
+            player += "<br/><img class='randomizer' src='/images/dice.jpeg' width=50px />";
+            $('.midst').html(player);
             $('.midst').fadeIn();
+            give_birth_to_fader();
         }  
     });
 };
 
-$(document).ready( function(){
-    $('.randomizer').click(function(){
-        $('.midst').fadeOut(somaAppears);
-    });
-});
+$(document).ready( give_birth_to_fader );
